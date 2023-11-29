ac642b029b7baf360063ec0b47ef1e03d17c9c09
Jayadityan Sethuraman <124813137+JishnuSethuraman@users.noreply.github.com>
2023-11-29 09:56:37 -0500

    Normalized matrix

--- loadDataTest.py
+++ loadDataTest.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_pickle('sensor_data/sensor_6632.pkl')
+#df = pd.read_pickle('sensor_data/sensor_6223.pkl')
+df = pd.read_pickle('combined/sensor_combined.pkl')
 print(df)
+
+plt.plot(df['value'])
--- timeFeatures.py
+++ timeFeatures.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 import os
+from sklearn.preprocessing import StandardScaler
 
 def process_data(file_path):
     # Read the csv file into a dataframe
@@ -28,7 +29,12 @@ def process_data(file_path):
 
     # Resample and average 'value' while keeping other columns as is
     for sensor_id, sensor_df in sensor_dfs.items():
-        sensor_dfs[sensor_id] = sensor_df.resample('T', on='timestamp').agg({'value': 'mean', 'hour': 'first', 'day_of_week': 'first', 'part_of_day': 'first'})
+        sensor_df = sensor_df.resample('T', on='timestamp').agg({'value': 'mean', 'hour': 'first', 'day_of_week': 'first', 'part_of_day': 'first'})
+
+        # Fill NaN values with 0
+        sensor_df['value'].fillna(0, inplace=True)
+
+        sensor_dfs[sensor_id] = sensor_df
 
     # Print the first 5 rows of each sensor's dataframe
     # for sensor_id, sensor_df in sensor_dfs.items():
@@ -45,63 +51,70 @@ def save_data(sensor_dfs, folder='sensor_data'):
         with open(os.path.join(folder, f'sensor_{sensor_id}.pkl'), 'wb') as file:
             pickle.dump(sensor_df, file)
 
+def align_sensors(sensor_dfs1, sensor_dfs2, sensor_ids):
+    # Find the latest start date and earliest end date among the selected sensors
+    latest_start = None
+    earliest_end = None
+
+    for sensor_id in sensor_ids:
+        sensor_df1 = sensor_dfs1.get(sensor_id)
+        sensor_df2 = sensor_dfs2.get(sensor_id)
+
+        # Determine the start and end dates from both datasets for the sensor
+        if sensor_df1 is not None:
+            start_date1, end_date1 = sensor_df1.index.min(), sensor_df1.index.max()
+            latest_start = max(latest_start, start_date1) if latest_start else start_date1
+            earliest_end = min(earliest_end, end_date1) if earliest_end else end_date1
+
+        if sensor_df2 is not None:
+            start_date2, end_date2 = sensor_df2.index.min(), sensor_df2.index.max()
+            latest_start = max(latest_start, start_date2) if latest_start else start_date2
+            earliest_end = min(earliest_end, end_date2) if earliest_end else end_date2
+
+    # Trim the dataframes in both datasets for the selected sensors
+    aligned_sensors = {}
+    for sensor_id in sensor_ids:
+        if sensor_id in sensor_dfs1:
+            aligned_sensors[sensor_id] = sensor_dfs1[sensor_id].loc[latest_start:earliest_end]
+        if sensor_id in sensor_dfs2:
+            aligned_sensors[sensor_id] = sensor_dfs2[sensor_id].loc[latest_start:earliest_end]
+
+    return aligned_sensors
+
+def normalize_and_aggregate(sensor_dfs, sensor_ids):
+    normalized_data = []
+
+    for sensor_id in sensor_ids:
+        # Select the 'value' column (already replaced NaNs with 0s)
+        values = sensor_dfs[sensor_id]['value'].values.reshape(-1, 1)
+
+        # Normalize the values
+        scaler = StandardScaler()
+        normalized_values = scaler.fit_transform(values)
+
+        # Create a dataframe from the normalized values
+        df_normalized = pd.DataFrame(normalized_values, columns=[f'value_{sensor_id}'], index=sensor_dfs[sensor_id].index)
+
+        normalized_data.append(df_normalized)
+
+    # Combine all normalized data into a single dataframe
+    combined_df = pd.concat(normalized_data, axis=1)
+
+    return combined_df
+
+
 def main():
-    file_path = r'D:\vscodefiles\MLIoTProject\human_activity_sensor_data_in_home_environment\human_activity_raw_sensor_data\sensor_sample_float.csv'
+    file_path = r'D:\vscodefiles\MLIoTProject\human_activity_sensor_data_in_home_environment\human_activity_raw_sensor_data\sensor_sample_int.csv'
+    file_path2 = r'D:\vscodefiles\MLIoTProject\human_activity_sensor_data_in_home_environment\human_activity_raw_sensor_data\sensor_sample_float.csv'
     print("Processing data...")
     sensor_dfs = process_data(file_path)
-    save_data(sensor_dfs)
+    sensor_dfs2 = process_data(file_path2)
+    selected_sensor_ids = [5896, 6686, 5892, 6687]  # List of sensor IDs you want to align
+    aligned_sensors = align_sensors(sensor_dfs, sensor_dfs2, selected_sensor_ids)
+    save_data(aligned_sensors)
+    normalized_combined_df = normalize_and_aggregate(aligned_sensors, selected_sensor_ids)
+    save_data({'combined': normalized_combined_df}, folder='combined')
 
 if __name__ == "__main__":
     main()
 
-# sensor_id_to_plot = '6634'  # Replace with an actual sensor_id
-
-# if sensor_id_to_plot in sensor_dfs:
-#     # Select one of the sensor dataframes to plot
-#     sensor_df_to_plot = sensor_dfs[sensor_id_to_plot]
-
-#     # Plotting
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(sensor_df_to_plot.index, sensor_df_to_plot['value'], label=f'Sensor {sensor_id_to_plot}')
-#     plt.xlabel('Time')
-#     plt.ylabel('Value')
-#     plt.title(f'Sensor Data over Time for Sensor ID {sensor_id_to_plot}')
-#     plt.legend()
-#     plt.show()
-# else:
-#     print(f"Sensor ID {sensor_id_to_plot} not found in the dataset.")
-
-# # device = torch.device("cuda:0")
-
-# class LSTMAttention(nn.Module):
-#     def __init__(self, num_features, hidden_size, seq_len, output_size):
-#         super(LSTMAttention, self).__init__()
-        
-#         self.num_features = num_features
-#         self.hidden_size = hidden_size
-#         self.seq_len = seq_len
-
-#         self.lstm = nn.LSTM(input_size=num_features, hidden_size=hidden_size, batch_first=True)
-#         self.attention = nn.Linear(hidden_size, seq_len)
-#         self.fc = nn.Linear(hidden_size * seq_len, output_size)
-
-#     def forward(self, x):
-#         lstm_out, _ = self.lstm(x)  # Shape of lstm_out: [batch_size, seq_len, hidden_size]
-#         attention_weights = torch.softmax(self.attention(lstm_out), dim=1)
-        
-#         # Apply attention weights
-#         attn_applied = torch.mul(lstm_out, attention_weights)
-
-#         # Reshape and apply fully connected layer
-#         fc_input = attn_applied.reshape(attn_applied.shape[0], -1)
-#         output = self.fc(fc_input)
-
-#         return output
-
-# # Assuming you know the number of features and the sequence length
-# num_features = len(df.columns)  # Number of features in your dataset
-# hidden_size = 64  # Example size
-# seq_len = 30  # Example sequence length
-# output_size = 1  # Predicting a single value
-
-# model = LSTMAttention(num_features, hidden_size, seq_len, output_size)
